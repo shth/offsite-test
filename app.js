ad850424f4ef4a90f4a22b0d2bf570d70714ce3f
Stephen Hung <stephenhung@astri.org>
2017-08-26 13:13:25 +0800

    install react pkgs and remove unwanted pkgs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -8,9 +7,6 @@ var index = require('./routes/index');
 
 var app = express();
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
