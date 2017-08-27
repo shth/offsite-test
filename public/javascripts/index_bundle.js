ec39ca43fae85add8c771a2b23fc5c393e288377
Stephen Hung <stephenhung@astri.org>
2017-08-27 14:56:49 +0800

    cleaned up unwanted view obj

--- public/javascripts/index_bundle.js
+++ public/javascripts/index_bundle.js
@@ -22516,17 +22516,6 @@ var viewList = [{
     date: '2017年1月27日'
 
 }, {
-    image: 'b5d1a9f06f0241bf42c3672ca90c53d3.jpg',
-
-    targetLink: 'https://www.hk01.com/01%E8%A7%80%E9%BB%9E/68137/-01%E8%A7%80%E9%BB%9E-%E5%8F%B0%E7%8D%A8%E5%B0%8E%E6%BC%94-%E7%8D%B2%E5%B9%B3%E5%8F%8D-%E5%BC%B5%E6%95%AC%E8%BB%92%E5%91%A2-',
-
-    title: '「台獨導演」獲平反　張敬軒呢 hihi？',
-
-    info: '陳玉勳執導的賀歲片電影《健忘村》，不但攝制預算大手筆、題材新穎，而且還結合大陸、台灣兩地資金與菁英人才，在台灣各地搭景拍攝，成為兩岸影業合作的最好範例。但此前，由於陳玉勳被指支持台……',
-
-    date: '2017年1月27日'
-
-}, {
     targetLink: "https://www.hk01.com/01%E8%A7%80%E9%BB%9E/68089/-01%E8%A7%80%E9%BB%9E-APA%E9%85%92%E5%BA%97%E9%9B%96%E6%92%A4%E5%8F%B3%E7%BF%BC%E6%9B%B8-%E8%BB%8D%E5%9C%8B%E4%B8%BB%E7%BE%A9%E9%99%B0%E9%AD%82%E6%9C%AA%E6%95%A3",
     image: "d7dd0992b84ec4f1620beda3b98402d9.jpg",
 
