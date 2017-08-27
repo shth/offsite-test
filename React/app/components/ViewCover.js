e4f0ce50c7828b3abe2da46b4f4ab9425687a625
Stephen Hung <stephenhung@astri.org>
2017-08-27 12:57:28 +0800

    setup complete: able to mount components onto DOM

---
+++ React/app/components/ViewCover.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ViewCover extends React.Component {
+    render(){
+        return (
+            <div>Im view cover</div>
+        )
+    }
+}
+export default ViewCover
---
+++ React/app/components/ViewList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ViewList extends React.Component {
+    render(){
+        return (
+            <div>Im view list</div>
+        )
+    }
+}
+export default ViewList
